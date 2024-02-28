1110d73c4dedfae62294751387f930ebdad12403
Jboy422 <gitonly543@gmail.com>
2024-02-28 09:58:48 +0900

    layout: slide pages

--- SlidingCreateIntro/ContentView.swift
+++ SlidingCreateIntro/ContentView.swift
@@ -1,24 +1,49 @@
-//
-//  ContentView.swift
-//  SlidingCreateIntro
-//
-//  Created by Jboy422 on 2024/02/28.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var pageIndex = 0
+    private let pages: [Page] = Page.samplePages
+    private let dotAppearance = UIPageControl.appearance()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView(selection: $pageIndex) {
+            ForEach(pages) { page in
+                VStack {
+                    Spacer()
+                    PageView(page: page)
+                    Spacer()
+                    if page == pages.last {
+                        Button("Sign up!", action: goToZero)
+                            .buttonStyle(.bordered)
+                    } else {
+                        Button("next", action: incrementPage)
+                            .buttonStyle(.borderedProminent)
+                    }
+                    Spacer()
+                }
+                .tag(page.tag)
+            }
         }
-        .padding()
+        .animation(.easeInOut, value: pageIndex)// 2
+        .indexViewStyle(.page(backgroundDisplayMode: .interactive))
+        .tabViewStyle(PageTabViewStyle())
+        .onAppear {
+            dotAppearance.currentPageIndicatorTintColor = .black
+            dotAppearance.pageIndicatorTintColor = .gray
+        }
+    }
+    
+    func incrementPage() {
+        pageIndex += 1
+    }
+    
+    func goToZero() {
+        pageIndex = 0
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
