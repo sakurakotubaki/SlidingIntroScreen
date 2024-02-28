188e45c2736a39a3bfdd6d75abc7232a0ad1b98b
Jboy422 <gitonly543@gmail.com>
2024-02-28 09:37:56 +0900

    Initial Commit

---
+++ SlidingCreateIntro/SlidingCreateIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlidingCreateIntroApp.swift
+//  SlidingCreateIntro
+//
+//  Created by Jboy422 on 2024/02/28.
+//
+
+import SwiftUI
+
+@main
+struct SlidingCreateIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
